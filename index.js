be8ff5c33662282595be1e075422c32cda991286
Venus Ang <venus@users.noreply.github.com>
2022-06-01 05:13:00 -0700

    added home page view

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
+
 app.use('/places', require('./controllers/places'));
 
 app.get('/', (req, res) => {
-  res.send('hello world');
+  res.render('home'); //render means the response renders the home module
 });
 
 app.get('*', (req, res) => {
