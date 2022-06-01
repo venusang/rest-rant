b46a827d2193b46eaced2c24826f608514543856
Venus Ang <venus@users.noreply.github.com>
2022-06-01 06:10:43 -0700

    added styles

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const express = require('express');
 const app = express();
 
 // MIDDLEWARE
+app.set('views', __dirname + '/views');
 app.set('view engine', 'jsx');
 app.engine('jsx', require('express-react-views').createEngine());
+app.use(express.static('public'));
 
 // ROUTES
 app.use('/places', require('./controllers/places'));
