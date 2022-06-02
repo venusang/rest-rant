48ee3d4e2b9ffa2ce4eb086d884c9a922c0f8bef
Venus Ang <venus@users.noreply.github.com>
2022-06-01 21:10:54 -0700

    Jazzed up form page

--- controllers/places.js
+++ controllers/places.js
@@ -1,3 +1,5 @@
+const { route } = require('express/lib/application');
+
 const router = require('express').Router();
 
 
@@ -7,6 +9,23 @@ router.get('/create', (req, res) => {
 
 router.get('/:id', (req, res) => {
   res.render('places/detail', { id: req.params.id });
+});
+
+router.post('/', (req, res) => {
+  let places = [{
+    name: 'H-Thai-ML',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Thai, Pan-Asian',
+    pic: '/images/thai-photo-1583475020839-105dddecc00a.jpg'
+  }, {
+    name: 'Coding Cat Cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Coffee, Bakery',
+    pic: '/images/coding-cat-cafe-istockphoto-962973582-1024x10.jpg'
+  }];
+  res.render('places/index', { places });
 });
 
 router.get('/', (req, res) => {
--- views/places/create.jsx
+++ views/places/create.jsx
@@ -6,8 +6,30 @@ function create_form() {
     <Def>
       <main>
         <h1>Add a new place</h1>
+        <form method="POST" action="/places">
+          <div className='form-group'>
+            <label htmlFor="name">Place Name</label>
+            <input className="form-control" id="name" name="name" />
+          </div>
+          <div className='form-group'>
+            <label htmlFor="pic">Place Picture</label>
+            <input className="form-control" type="url" id="pic" name="pic" />
+          </div>
+          <div className='form-group'>
+            <label htmlFor="city">Place City</label>
+            <input className="form-control" id="city" name="city" />
+          </div>
+          <div className='form-group'>
+            <label htmlFor="state">Place State</label>
+            <input className="form-control" id="state" name="state" />
+          </div>
+          <div className='form-group'>
+            <label htmlFor="cuisines">Place Cuisines</label>
+            <input className="form-control" id="cuisines" name="cuisines" required />
+          </div>
+          <input className="btn btn-primary" type="submit" value="Add Place" />
+        </form>
       </main>
-      {/* create form goes here */}
     </Def>
   )
 }
