e089d5a78f5c097fec0a3d68f9aeeff764b80781
Venus Ang <venus@users.noreply.github.com>
2022-10-02 12:10:59 -0700

    end of REST-Ran Part 5 (be-4-rest-rant-part-5) added bootstrap classes to clean-up form styling

--- views/places/create.jsx
+++ views/places/create.jsx
@@ -14,28 +14,36 @@ function create_form({ states }) {
       <main>
         <h1>Add a new place</h1>
         <form className="form-horizontal" method="POST" action="/places">
-          <div className="form-group">
+          <div className="form-group row mb-3">
             <label className="col-sm-2 control-label" htmlFor="name">Place Name</label>
             <div class="col-sm-10">
               <input className="form-control" id="name" name="name" required />
             </div>
           </div>
-          <div className="form-group">
+          <div className="form-group row mb-3">
             <label className="col-sm-2 control-label" htmlFor="pic">Place Picture</label>
             <div class="col-sm-10">
               <input className="form-control" type="url" id="pic" name="pic" />
             </div>
           </div>
-          <select className='form-control' id="state" name="state">
-            {stateOptions}
-          </select>
-          <div className="form-group">
+          <div className="form-group row mb-3">
+            <label className="col-sm-2 control-label" htmlFor="state">State</label>
+            <div class="col-sm-10">
+              <select className='form-select' id="state" name="state">
+                <option selected>Select a state</option>
+                {stateOptions}
+              </select>
+            </div>
+          </div>
+          <div className="form-group row mb-3">
             <label className="col-sm-2 control-label" htmlFor="cuisines">Place Cuisines</label>
             <div class="col-sm-10">
               <input className="form-control" id="cuisines" name="cuisines" required />
             </div>
           </div>
-          <input className="btn btn-primary" type="submit" value="Add Place" />
+          <div className="form-group mb-3 text-center">
+            <input className="btn btn-primary" type="submit" value="Add Place" />
+          </div>
         </form>
       </main>
     </Def >
