9bdebbc207640137e1249b0abbc85ea32f64af17
Venus Ang <venus@users.noreply.github.com>
2022-10-02 11:45:02 -0700

    added states parameter to places create form

--- views/places/create.jsx
+++ views/places/create.jsx
@@ -1,36 +1,44 @@
 const React = require('react');
 const Def = require('../default');
 
-function create_form() {
+
+function create_form({ states }) {
+  const stateOptions = states.map((state) => {
+    return (
+      <option value={state.abbreviation}>{state.name}</option>
+    )
+  });
+
   return (
     <Def>
       <main>
         <h1>Add a new place</h1>
-        <form method="POST" action="/places">
-          <div className='form-group'>
-            <label htmlFor="name">Place Name</label>
-            <input className="form-control" id="name" name="name" required />
+        <form className="form-horizontal" method="POST" action="/places">
+          <div className="form-group">
+            <label className="col-sm-2 control-label" htmlFor="name">Place Name</label>
+            <div class="col-sm-10">
+              <input className="form-control" id="name" name="name" required />
+            </div>
           </div>
-          <div className='form-group'>
-            <label htmlFor="pic">Place Picture</label>
-            <input className="form-control" type="url" id="pic" name="pic" />
+          <div className="form-group">
+            <label className="col-sm-2 control-label" htmlFor="pic">Place Picture</label>
+            <div class="col-sm-10">
+              <input className="form-control" type="url" id="pic" name="pic" />
+            </div>
           </div>
-          <div className='form-group'>
-            <label htmlFor="city">Place City</label>
-            <input className="form-control" id="city" name="city" />
-          </div>
-          <div className='form-group'>
-            <label htmlFor="state">Place State</label>
-            <input className="form-control" id="state" name="state" />
-          </div>
-          <div className='form-group'>
-            <label htmlFor="cuisines">Place Cuisines</label>
-            <input className="form-control" id="cuisines" name="cuisines" required />
+          <select className='form-control' id="state" name="state">
+            {stateOptions}
+          </select>
+          <div className="form-group">
+            <label className="col-sm-2 control-label" htmlFor="cuisines">Place Cuisines</label>
+            <div class="col-sm-10">
+              <input className="form-control" id="cuisines" name="cuisines" required />
+            </div>
           </div>
           <input className="btn btn-primary" type="submit" value="Add Place" />
         </form>
       </main>
-    </Def>
+    </Def >
   )
 }
 
